8652317679e769d48d36b1f80c4d29eebfac2697
Adrian Ioan <adrian.ioan@conkergroup.com>
2021-05-14 23:57:37 +0100

    Gets rid of http client what was I thinking oh god oh fuck

--- lib/environment.py
+++ lib/environment.py
@@ -7,5 +7,6 @@ load_dotenv()
 class Environment:
     DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
     DISCORD_GUILD = os.getenv('DISCORD_GUILD')
-    NETFLIX_API_BASE_URL = os.getenv('NETFLIX_API_BASE_URL')
+    NETFLIX_API_DOMAIN = os.getenv('NETFLIX_API_DOMAIN')
     NETFLIX_API_KEY = os.getenv('NETFLIX_API_KEY')
+    UK_CODE = os.getenv('UK_CODE')
--- lib/netflix_api_util.py
+++ lib/netflix_api_util.py
@@ -7,18 +7,22 @@ from models.search_response import SearchResponse
 class NetflixAPIUtil:
 
     def __init__(self):
-        self.http_client = http.client.HTTPSConnection(Environment.NETFLIX_API_BASE_URL)
+        self.base_url = f'https://{Environment.NETFLIX_API_DOMAIN}'
+
         self.headers = {
             'x-rapidapi-key': Environment.NETFLIX_API_KEY,
-            'x-rapidapi-host': Environment.NETFLIX_API_BASE_URL
+            'x-rapidapi-host': Environment.NETFLIX_API_DOMAIN
         }
 
     def search(self, query: str) -> SearchResponse:
-        self.http_client.request(method="GET", headers=self.headers, url=f'/aaapi.cgi?q={query}')
-        res = self.http_client.getresponse()
-        search_response = SearchResponse.parse_raw(res.read().decode('utf-8'))
+
+        query_params = {
+            'limit': '5',
+            'countryList': Environment.UK_CODE,
+            'query': query
+        }
+
+        raw_response = requests.get(url=f'{self.base_url}/search', headers=self.headers, params=query_params)
+        search_response = SearchResponse.parse_raw(raw_response.text)
 
         return search_response
-
-    def __del__(self):
-        self.http_client.close()
